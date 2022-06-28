156a783ce1d8b76a302db070029f4c3946af585f
Atsushi0915 <w_atsushi_0915@outlook.jp>
2022-06-28 19:35:02 +0900

    CROSファイル修正

--- frontend/src/urls/urls.js
+++ frontend/src/urls/urls.js
@@ -1,4 +1,4 @@
-const DEFAULT_API_URL = 'http://localhost:3000/api/v1'
+const DEFAULT_API_URL = 'https://atsushi-todolist-pf.herokuapp.com/api/v1'
 
 export const taskIndexUrl = `${DEFAULT_API_URL}/tasks`
 
